eaa4c6471b4a157f2de79d98f37da7d569978594
deprus <deprus97@gmail.com>
2025-02-17 01:10:24 +0700

    feat: Add background component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Background from "@/components/background";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} font-inter antialiased`}>
+        <Background />
         <div className="flex flex-col min-h-screen">
           <Header />
           <main className="flex-1 flex container">{children}</main>
